b477c40261287ee546905052f8e0e46bd380d46d
Ardhan <olivier_fournier@anciens-mss.ca>
2017-01-04 02:06:08 -0500

    Items replaced

--- Assets/Script/Items.cs
+++ Assets/Script/Items.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Items : MonoBehaviour {
+    //is the item a bonus
+    private bool isBonus;
+    //what type of item
+    private int type;
 
-	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		
 	}
