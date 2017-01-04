a807cc019790aca40300b71106ccea8e435e3722
unknown <samuellalibel@hotmail.com>
2017-01-04 00:30:44 -0500

    Début splitCamera

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     int cycles,
-        playerNumber;
+        playerNumber = 2;
     Timer timer;
     private GameObject instanciatedObject;
     private List<Character> player = new List<Character>();
     private GameObject menuCamera;
+    public GameObject playerPrefab;
+    private Transform cameraPos;
 
     private void Awake()
     {
         menuCamera = GameObject.Find("Menu Camera");
         setStartLevel();
+        DestroyImmediate(GameObject.Find("Menu Camera"));
     }
 
 
@@ -24,9 +27,14 @@ public class GameManager : MonoBehaviour
         cycles = 2;
         timer = GetComponent<Timer>();
         timer.StartTimer();
+        cameraPos = Camera.main.transform;
         for (int i = 0; i < playerNumber; i++)
         {
-            //player.Add(instanciatedObject.GetComponent<Character>());
+            instanciatedObject = (GameObject)Instantiate(playerPrefab, new Vector3(cameraPos.position.x, cameraPos.position.y, cameraPos.position.z+2), Quaternion.identity);
+            //instanciatedObject.transform.parent.position = new Vector3(0,0,0);
+            instanciatedObject.name = "Player_" + i;
+            player.Add(instanciatedObject.GetComponent<Character>());
+            player[i].GetComponent<Character>().playerNumberB = i;
         }
     }
 
