c9e8062af5d978f36409e9d5e0d47e2b525d7542
Ardhan <olivier_fournier@anciens-mss.ca>
2017-01-04 10:53:01 -0500

    Buttons begun

---
+++ Assets/Script/Button.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+    public GameObject[] image;
+    private int noPlayer;
+    public int key;
+
+    public Button (int player)
+    {
+        noPlayer = player;
+        key = Random.Range(0, 4);
+    }
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
