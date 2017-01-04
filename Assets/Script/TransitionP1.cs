3ce95347d3e5f1a3ce07eb4d13950ba44d2060f8
Ardhan <olivier_fournier@anciens-mss.ca>
2017-01-04 15:45:39 -0500

    Fighting transition

---
+++ Assets/Script/TransitionP1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionP1 : MonoBehaviour {
+    private float time,
+                  speed;
+    // Use this for initialization
+    void Start() {
+        time = 0f;
+        speed = 0f;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.position += Vector3.down * vitesseDefilement() * Time.deltaTime;
+    }
+
+    float vitesseDefilement()
+    {
+        return 1 * (Time.time - 5) + 0.5f;
+    }
+}
---
+++ Assets/TransitionP2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionP2 : MonoBehaviour
+{
+    private float time,
+                  speed;
+    // Use this for initialization
+    void Start()
+    {
+        time = 0f;
+        speed = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.down * vitesseDefilement() * Time.deltaTime;
+    }
+
+    float vitesseDefilement()
+    {
+        return -1.5f * (Time.time - 5f) + -0.5f;
+    }
+}
