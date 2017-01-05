3ebee2b49bb16bcc054dbd22b2fdd9a5d1f05084
DESKTOP-BU2D1OP\ASUS <jessicarsenault@gmail.com>
2017-01-05 17:03:21 -0500

    Fight avec le robot

--- Assets/Script/TransitionP1.cs
+++ Assets/Script/TransitionP1.cs
@@ -28,11 +28,18 @@ public class TransitionP1 : MonoBehaviour {
         else
         {
             transform.position += Vector3.up * 9 * Time.deltaTime;
+            StartCoroutine(wait());
         }
     }
 
     void vitesseDefilement()
     {
         speed = System.Math.Abs(0.9f * (Time.time - 5f));
+    }
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(2);
+        GameObject.Find("FightIntro").SetActive(false);
     }
 }
