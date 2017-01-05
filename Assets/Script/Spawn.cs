b2405ec5b2b7aa89c20cb018e59bbc41540d8526
unknown <samuellalibel@hotmail.com>
2017-01-05 16:36:12 -0500

    Cycles en cours de fonctionnement... manque le player 1 a bouger

--- Assets/Script/Spawn.cs
+++ Assets/Script/Spawn.cs
@@ -8,12 +8,12 @@ public class Spawn : MonoBehaviour {
 
     // Use this for initialization
     void Start() {
-        switch (playerSide)
+        /*switch (playerSide)
         {
             case 0:  GameObject.Find("ManagerObject").GetComponent<GameManager>().SpawnPointP1.Add(this.transform.gameObject); break;
             case 1:  GameObject.Find("ManagerObject").GetComponent<GameManager>().SpawnPointP2.Add(this.transform.gameObject); break;
             default: break;
-        }
+        }*/
 	}
 	
 	// Update is called once per frame
