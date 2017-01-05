445a912fd66569c1c22fa9133d254983a4fe9f04
unknown <samuellalibel@hotmail.com>
2017-01-05 13:10:27 -0500

    Loop de jeu en continuations, props mis à jour,

--- Assets/Script/END.cs
+++ Assets/Script/END.cs
@@ -5,12 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class END : MonoBehaviour {
 
+    int[] Wins = new int[2];
 	// Use this for initialization
 	void Awake ()
     {
         Destroy(GameObject.Find("Player_0"));
         Destroy(GameObject.Find("Player_1"));
-
+        Wins[0]=GameManager.instance.player[0].wins;
+        Wins[1] = GameManager.instance.player[1].wins;
+        Destroy(GameObject.Find("ManagerObject"));
     }
 	
 	// Update is called once per frame
@@ -20,7 +23,6 @@ public class END : MonoBehaviour {
 
     public void restartGame()
     {
-        Destroy(GameObject.Find("ManagerObject"));
         SceneManager.LoadScene("Menu");
     }
 
